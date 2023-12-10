d4f814a4dd04eeeefe78554e9a29faa43de64295
Newyork167 <newyork167@gmail.com>
2023-12-10 12:49:31 -0500

    Initial Commit

---
+++ LethalCompanyTemplate/Plugin.cs
@@ -0,0 +1,16 @@
+﻿using BepInEx;
+
+namespace LethalCompanyTemplate
+{
+    
+    [BepInPlugin("org.bepinex.plugins.exampleplugin", "Example Plug-In", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private string PLUGIN_GUID = "org.bepinex.plugins.exampleplugin";
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+        }
+    }
+}
---
+++ LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
@@ -0,0 +1,9 @@
+namespace LethalCompanyTemplate
+{
+    public static class PluginInfo
+    {
+        public const string PLUGIN_GUID = "LethalCompanyTemplate";
+        public const string PLUGIN_NAME = "LethalCompanyTemplate";
+        public const string PLUGIN_VERSION = "1.0.0";
+    }
+}
