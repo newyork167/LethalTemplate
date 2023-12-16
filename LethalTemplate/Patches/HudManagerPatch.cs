dc3f72c474e97f2962b77640f53ae05f4d644df8
Newyork167 <newyork167@gmail.com>
2023-12-15 23:23:34 -0500

    Auto stash before revert of "Changed naming convention"

---
+++ LethalTemplate/Patches/HudManagerPatch.cs
@@ -0,0 +1,19 @@
+﻿using HarmonyLib;
+using UnityEngine.InputSystem;
+
+namespace LethalCompanyTemplate.Patches
+{
+    [HarmonyPatch]
+    internal class HUDManagerPatch
+    {
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(HUDManager), "PingScan_performed")]
+        private static bool DebugScan(InputAction.CallbackContext context)
+        {
+            Plugin.Log.LogInfo("Ping scan performed");
+            Plugin.Log.LogError($"{context}");
+            
+            return true;
+        }
+    }
+}
---
+++ LethalTemplate/Plugin.cs
@@ -0,0 +1,21 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using UnityEngine;
+
+namespace LethalTemplate
+{
+    
+    [BepInPlugin("org.newyork167.plugins.lethaltemplate", "Example Plug-In", "1.0.0.0")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private string PLUGIN_GUID = "org.newyork167.plugins.lethaltemplate";
+        
+        internal static ManualLogSource Log;
+        private void Awake()
+        {
+            // Plugin startup logic
+            Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            Logger.LogError($"TESTING ERROR LOG");
+        }
+    }
+}
