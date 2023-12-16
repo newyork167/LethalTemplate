f54d4d898b1c26e87e03315f46b4186d0de6c3b4
Newyork167 <newyork167@gmail.com>
2023-12-15 21:40:28 -0500

    Removed bin/obj files

--- LethalCompanyTemplate/obj/Debug/netstandard2.1/PluginInfo.cs
+++
@@ -1,9 +0,0 @@
-namespace LethalCompanyTemplate
-{
-    public static class PluginInfo
-    {
-        public const string PLUGIN_GUID = "LethalCompanyTemplate";
-        public const string PLUGIN_NAME = "LethalCompanyTemplate";
-        public const string PLUGIN_VERSION = "1.0.0";
-    }
-}
