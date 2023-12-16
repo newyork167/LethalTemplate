69597d3f2c023e684d981588896d11c64eb4031d
Newyork167 <newyork167@gmail.com>
2023-12-15 21:37:04 -0500

    Main Plugin Updates

--- LethalCompanyTemplate/Plugin.cs
+++ LethalCompanyTemplate/Plugin.cs
@@ -3,14 +3,15 @@
 namespace LethalCompanyTemplate
 {
     
-    [BepInPlugin("org.bepinex.plugins.exampleplugin", "Example Plug-In", "1.0.0.0")]
+    [BepInPlugin("org.newyork167.plugins.lethaltemplate", "Example Plug-In", "1.0.0.0")]
     public class Plugin : BaseUnityPlugin
     {
-        private string PLUGIN_GUID = "org.bepinex.plugins.exampleplugin";
+        private string PLUGIN_GUID = "org.newyork167.plugins.lethaltemplate";
         private void Awake()
         {
             // Plugin startup logic
             Logger.LogInfo($"Plugin {PLUGIN_GUID} is loaded!");
+            Logger.LogError($"TESTING ERROR LOG");
         }
     }
 }
