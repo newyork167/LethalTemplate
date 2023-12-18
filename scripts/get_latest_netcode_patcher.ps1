bff7e47c8d7fabb7ec04316b515786e2c1eacd5a
Newyork167 <newyork167@gmail.com>
2023-12-17 23:36:55 -0500

    Removed temp repo submodule

--- scripts/get_latest_netcode_patcher.ps1
+++ scripts/get_latest_netcode_patcher.ps1
@@ -5,6 +5,8 @@ $solution_dir = $args[0]
 $latest_release = Invoke-RestMethod https://github.com/EvaisaDev/UnityNetcodeWeaver/releases/latest | grep -i "NetcodePatcher-*" | head -n 1 | awk '{print $2; }'
 $latest_release_number = $latest_release.Split('-')[-1]
 
+Write-Host "Latest NetcodePatcher release: $latest_release_number"
+
 # Check if "$solution_dir\tools\$latest_release.zip" exists
 if (Test-Path "$solution_dir\tools\$latest_release.zip") {
     Write-Host "NetcodePatcher $latest_release already downloaded"
@@ -12,12 +14,14 @@ if (Test-Path "$solution_dir\tools\$latest_release.zip") {
 }
 
 # If not, download it
-$download_url = "https://github.com/EvaisaDev/UnityNetcodeWeaver/releases/download/$latest_release_number/$latest_release_number.zip"
+$download_url = "https://github.com/EvaisaDev/UnityNetcodeWeaver/releases/download/$latest_release_number/$latest_release.zip"
 $download_path = "$solution_dir\tools\$latest_release.zip"
+Write-Host "Downloading NetcodePatcher $latest_release from $download_url to $download_path"
 Invoke-WebRequest -Uri $download_url -OutFile $download_path
 
-# Unzip download_path to $solution_dir\tools
-Expand-Archive -Path $download_path -DestinationPath "$solution_dir\tools"
+# Unzip download_path to $solution_dir\tools\$latest_release
+Write-Host "Unzipping $download_path to $solution_dir\tools\$latest_release_number"
+Expand-Archive -Path $download_path -DestinationPath "$solution_dir\tools\NetcodePatcher-$latest_release_number" -Force
 
 # Remove "$solution_dir\tools\NetcodePatcher" if it exists
 if (Test-Path "$solution_dir\tools\NetcodePatcher") {
@@ -26,3 +30,15 @@ if (Test-Path "$solution_dir\tools\NetcodePatcher") {
 
 # rename to NetcodePatcher 
 Rename-Item -Path "$solution_dir\tools\NetcodePatcher-$latest_release_number" -NewName "$solution_dir\tools\NetcodePatcher"
+
+# Get-Content .\LethalTemplate\LethalTemplate.csproj and grab a single line that contains \Lethal Company_Data\Managed
+$hint_path = Get-Content .\LethalTemplate\LethalTemplate.csproj | Select-String -Pattern "Lethal Company_Data\\Managed" | Select-Object -First 1
+
+# Replace everything up to and including <HintPath> with an empty string, and everything </HintPath> and after with an empty string
+$hint_path = $hint_path -replace ".*<HintPath>", ""
+$hint_path = $hint_path -replace "</HintPath>.*", ""
+$lethal_company_managed_path = $hint_path.Substring(0, $hint_path.LastIndexOf("\"))
+
+# Copy everything from $lethal_company_managed_path to $solution_dir\tools\NetcodePatcher\deps
+Write-Host "Copying $lethal_company_managed_path to $solution_dir\tools\NetcodePatcher\deps"
+Copy-Item -Path "$lethal_company_managed_path\*" -Destination "$solution_dir\tools\NetcodePatcher\deps" -Recurse -Force
